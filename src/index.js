ef4f2683acc929c4be4e3d0ec57baa49894a13a6
dstyle0210 <dstyle0210@gmail.com>
2022-01-17 10:27:07 +0900

    웹팩 재공부 2일차

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import _ from 'lodash';
-import './style.css';
+import printMe from './print.js';
+
 function component() {
-    const element = document.createElement('div');
+  const element = document.createElement('div');
+  const btn = document.createElement('button');
+
+  element.innerHTML = _.join(['Hello', 'webpack3'], ' ');
   
-    // 이 라인이 동작하려면 현재 스크립트를 통해 포함된 Lodash가 필요합니다.
-    element.innerHTML = _.join(['Hello', 'webpack2'], ' ');
-    element.classList.add('hello');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+  return element;
+};
+
+document.body.appendChild(component());
